d3c35f3391a4fb9ec5218b1f74835ea9e31f565c
Harshad Prajapati <prajapatiplants1982@gmail.com>
2026-07-19 14:08:46 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,41 +1,14 @@
 import requests
-import subprocess
-import os
-from ftplib import FTP
 
-# CSV URL
-csv_url = "https://businessflow360.infinityfree.me/files/sales/sales_all.csv"
+url = "https://businessflow360.infinityfree.me/files/sales/sales_all.csv"
 
-# CSV download
-response = requests.get(csv_url)
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
 
-with open("sales_all.csv","wb") as file:
-    file.write(response.content)
+response = requests.get(url, headers=headers, timeout=30)
 
-print("CSV downloaded")
+print(response.status_code)
 
-# sales chart generate
-subprocess.run([
-    "python",
-    "sales_chart.py"
-])
-
-print("Sales chart generated")
-
-ftp = FTP(os.environ["FTP_HOST"])
-
-ftp.login(
-    os.environ["FTP_USER"],
-    os.environ["FTP_PASS"]
-)
-
-with open("sales_chart.png","rb") as file:
-
-    ftp.storbinary(
-        "STOR /htdocs/charts/sales_chart/sales_chart.png",
-        file
-    )
-
-ftp.quit()
-
-print("Chart uploaded")
+with open("sales_all.csv", "wb") as f:
+    f.write(response.content)
