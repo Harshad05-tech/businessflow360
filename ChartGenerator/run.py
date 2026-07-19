cc55a7aedcd8dd322534b64e76aaa89e81847fcc
Harshad Prajapati <prajapatiplants1982@gmail.com>
2026-07-19 09:18:15 +0530

    Update run.py

--- ChartGenerator/run.py
+++ ChartGenerator/run.py
@@ -1,1 +1,22 @@
+import requests
+import subprocess
+import os
 
+# CSV URL
+csv_url = "https://businessflow360.infinityfree.me/files/sales/sales_all.csv"
+
+# CSV download
+response = requests.get(csv_url)
+
+with open("sales_all.csv","wb") as file:
+    file.write(response.content)
+
+print("CSV downloaded")
+
+# sales chart generate
+subprocess.run([
+    "python",
+    "sales_chart.py"
+])
+
+print("Sales chart generated")
