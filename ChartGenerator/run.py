cfb598a3206e42e5bc9e751a4987fe1c7b5cb10c
Harshad Prajapati <prajapatiplants1982@gmail.com>
2026-07-19 09:47:43 +0530

    Implement FTP upload for sales chart
    Added functionality to upload the generated sales chart to an FTP server.

--- ChartGenerator/run.py
+++ ChartGenerator/run.py
@@ -1,6 +1,7 @@
 import requests
 import subprocess
 import os
+from ftplib import FTP
 
 # CSV URL
 csv_url = "https://businessflow360.infinityfree.me/files/sales/sales_all.csv"
@@ -20,3 +21,21 @@ subprocess.run([
 ])
 
 print("Sales chart generated")
+
+ftp = FTP(os.environ["FTP_HOST"])
+
+ftp.login(
+    os.environ["FTP_USER"],
+    os.environ["FTP_PASS"]
+)
+
+with open("sales_chart.png","rb") as file:
+
+    ftp.storbinary(
+        "STOR /htdocs/charts/sales_chart/sales_chart.png",
+        file
+    )
+
+ftp.quit()
+
+print("Chart uploaded")
